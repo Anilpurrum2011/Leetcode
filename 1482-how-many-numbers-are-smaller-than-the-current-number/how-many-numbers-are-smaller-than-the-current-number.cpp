e8242c2f5a4004232f7cc42ac15717c49a8f65ea
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-03-18 19:17:06 +0530

    Time: 5 ms (49.83%) | Memory: 14.7 MB (15.48%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,17 +1,35 @@
+// class Solution {
+// public:
+//     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+//        int n=nums.size();
+//        vector<int>res;
+//        for(int i=0;i<n;i++){
+//         int count=0;
+//         for(int j=0;j<n;j++){
+//             if(nums[i]>nums[j]){
+//                 count++;
+//             }
+//         }
+//         res.push_back(count);
+//        }
+//        return res;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-       int n=nums.size();
-       vector<int>res;
-       for(int i=0;i<n;i++){
-        int count=0;
-        for(int j=0;j<n;j++){
-            if(nums[i]>nums[j]){
-                count++;
-            }
-        }
-        res.push_back(count);
-       }
-       return res;
+    int  n=nums.size();
+    unordered_map<int,int>maps;
+    vector<int>res=nums;
+    sort(res.begin(),res.end());
+    for(int i=n-1;i>=0;i--){
+        maps[res[i]]=i;
+    }
+    for(int i=0;i<n;i++){
+        nums[i]=maps[nums[i]];
+    }
+    return nums;
     }
 };
