279fbfa34de3d8739e40a7b00c88ee7f31fa6ec7
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-03-18 19:36:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (86.56%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -1,40 +1,40 @@
-// class Solution {
-// public:
-//     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-//         int n=points.size();
-//         int x1=points[0][0];
-//         int y1=points[0][1];
-//         int res=0;
-//         for(int i=1;i<n;i++){
-//             int x2=points[i][0];
-//             int y2=points[i][1];
-//             res+=max(abs(x2-x1),abs(y2-y1));
-//             x1=x2;
-//             y1=y2;
-//         }
-//         return res;
-        
-//     }
-// };
-
-
 class Solution {
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-       int n=points.size();
-       int t=0;
-       for(int i=0;i<n-1;i++){
-        vector<int>a(points[i]);
-        vector<int>b(points[i+1]);
-        int x=abs(a[0]-b[0]);
-        int y=abs(a[1]-b[1]);
-        if(x>y){
-            t+=x;
+        int n=points.size();
+        int x1=points[0][0];
+        int y1=points[0][1];
+        int res=0;
+        for(int i=1;i<n;i++){
+            int x2=points[i][0];
+            int y2=points[i][1];
+            res+=max(abs(x2-x1),abs(y2-y1));
+            x1=x2;
+            y1=y2;
         }
-        else{
-            t+=y;
-        }
-       }
-       return t;
+        return res;
+        
     }
 };
+
+
+// class Solution {
+// public:
+//     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+//        int n=points.size();
+//        int t=0;
+//        for(int i=0;i<n-1;i++){
+//         vector<int>a(points[i]);
+//         vector<int>b(points[i+1]);
+//         int x=abs(a[0]-b[0]);
+//         int y=abs(a[1]-b[1]);
+//         if(x>y){
+//             t+=x;
+//         }
+//         else{
+//             t+=y;
+//         }
+//        }
+//        return t;
+//     }
+// };
