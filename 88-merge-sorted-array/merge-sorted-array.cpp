cdaca3b909517f42369f9585f59d4927ea519838
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-03 11:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (39.28%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,70 @@
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         int a = m - 1;
+//         int b = n - 1;
+//         int c= m + n - 1;
+//         while (b >= 0) {
+//             if (a >= 0 && nums1[a] > nums2[b]) {
+//                 nums1[c--] = nums1[a--];
+//             } else {
+//                 nums1[c--] = nums2[b--];
+//             }
+//         }
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+          int a=nums1.size();
+          int b=nums2.size();
+          int j=0;
+          for(int i=0;i<a;i++){
+            if(nums1[i]==0 && j<n){
+                nums1[i]=nums2[j];
+                j++;
+            }
+          }
+          sort(nums1.begin(),nums1.end());
+     }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
