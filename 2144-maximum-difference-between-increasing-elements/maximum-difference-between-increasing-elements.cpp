a9cb375891919f1bf4b51cdd3b9168c15d12f881
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-07 15:15:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (67.31%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
@@ -1,19 +1,35 @@
+// class Solution {
+// public:
+//     int maximumDifference(vector<int>& nums) {
+//         int n=nums.size();
+//         int res;
+//         for(int i=0;i<n;i++)
+//         { 
+//             for(int j=1;j<n;j++){
+//             if(nums[j]>nums[i] && j>i){
+//                 res=max(res,nums[j]-nums[i]);
+//             }
+//           }
+//         }
+//         if(res>0){
+//             return res;
+//         }
+//         return -1;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
-        int n=nums.size();
-        int res;
-        for(int i=0;i<n;i++)
-        { 
-            for(int j=1;j<n;j++){
-            if(nums[j]>nums[i] && j>i){
-                res=max(res,nums[j]-nums[i]);
-            }
-          }
+    int maxdiff=-1,minele=nums[0];
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]>minele){
+            maxdiff=max(maxdiff,nums[i]-minele);
         }
-        if(res>0){
-            return res;
-        }
-        return -1;
+        minele=min(minele,nums[i]);
+    }  
+    return maxdiff;
     }
 };
