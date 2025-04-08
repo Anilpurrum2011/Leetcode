23e22e43039004c693b256fd95452f8c8b264a7a
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-08 14:27:32 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.79%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -31,18 +31,50 @@
 
 
 
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//     int  n=nums.size();
+//     int k=target;
+//     int left=0;
+//     int right=n-1;
+//     while(left<=right){
+//         int mid=left+(right-left)/2;
+//         if(nums[mid]==k)return mid;
+//         if(nums[left]<=nums[mid]){
+//             if(nums[left]<=k && k<=nums[mid]){
+//                 right=mid-1;
+//             }
+//             else{
+//                 left=mid+1;
+//             }
+//         }
+//         else{
+//             if(nums[mid]<=k && k<=nums[right]){
+//                 left=mid+1;
+//             }
+//             else{
+//                 right=mid-1;
+//             }
+//         }
+//     }
+//     return -1;
+
+//     }
+// };
+
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-    int  n=nums.size();
-    int k=target;
-    int left=0;
-    int right=n-1;
-    while(left<=right){
+      int n=nums.size();
+      int left=0;
+      int right=n-1;
+      while(left<=right){
         int mid=left+(right-left)/2;
-        if(nums[mid]==k)return mid;
+        if(nums[mid]==target) return mid;
         if(nums[left]<=nums[mid]){
-            if(nums[left]<=k && k<=nums[mid]){
+            if(nums[left]<=target && target<=nums[mid]){
                 right=mid-1;
             }
             else{
@@ -50,15 +82,14 @@ public:
             }
         }
         else{
-            if(nums[mid]<=k && k<=nums[right]){
+            if(nums[mid]<=target && target<=nums[right]){
                 left=mid+1;
             }
             else{
                 right=mid-1;
             }
         }
-    }
-    return -1;
-
+      }
+      return -1;
     }
 };
