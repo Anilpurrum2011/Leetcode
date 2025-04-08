9b74d3353473bbbcbbbabbe1326fe813c60b0a80
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-08 10:30:52 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (53.42%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -12,8 +12,28 @@
 //             }
 //         }
 //         return maxi;
+// //     }
+// // };
+
+
+
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//     int n=nums.size();
+//     int sum=0;
+//     int maxsum=INT_MIN;
+//     for(int i=0;i<n;i++){
+//         sum+=nums[i];
+//         maxsum=max(maxsum,sum);
+//         if(sum<0){
+//             sum=0;
+//         }
+//     }
+//     return maxsum;
 //     }
 // };
+
 
 
 
@@ -22,7 +42,7 @@ public:
     int maxSubArray(vector<int>& nums) {
     int n=nums.size();
     int sum=0;
-    int maxsum=INT_MIN;
+    int maxsum;
     for(int i=0;i<n;i++){
         sum+=nums[i];
         maxsum=max(maxsum,sum);
@@ -33,7 +53,3 @@ public:
     return maxsum;
     }
 };
-
-
-
-
