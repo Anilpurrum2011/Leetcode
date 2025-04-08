63bd67573cc91eddac91dbab17b6035789e3021b
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-08 22:22:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (90.41%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -1,13 +1,25 @@
+// class Solution {
+// public:
+//     string convertToTitle(int columnNumber) {
+//         string res="";
+//         while(columnNumber>0){
+//             columnNumber--;
+//             res=char((columnNumber%26)+'A')+res;
+//             columnNumber/=26;
+//         }
+//         return res;
+//     }
+// };
+
+
+
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
-        string res="";
-        while(columnNumber>0){
-            columnNumber--;
-            res=char((columnNumber%26)+'A')+res;
-            columnNumber/=26;
-        }
-        return res;
+     if(columnNumber==0)return "";
+     columnNumber--;
+     char c='A'+(columnNumber%26);
+     return convertToTitle(columnNumber/26)+c;
     }
 };
 
