96040f33b4d96f041deb839af653a1856601a557
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-16 23:02:40 +0530

    Time: 64 ms (96.68%) | Memory: 78.9 MB (93.36%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -5,7 +5,7 @@ public:
         for (int i = 0, left = 0 ; i < nums.size() ; i++) {
             k -= mpp[nums[i]]++;
             while (k <= 0) k += --mpp[nums[left++]];
-            cnt += left;
+            cnt =cnt+ left;
         }
         return cnt;
     }
