a5abcf5370e260162006e8fb7cf7225769a6d616
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-16 09:45:51 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.43%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -33,20 +33,39 @@
 // };
 
 
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if(n==0 || n==1){
+//             return 1;
+//         }
+//         int first=1,second=1;
+//         int total;
+//         for(int i=2;i<n+1;i++){
+//             total=first+second;
+//             first=second;
+//             second=total;
+//         }
+//        return total;
+//     }
+// };
+
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==0 || n==1){
-            return 1;
-        }
-        int first=1,second=1;
-        int total;
-        for(int i=2;i<n+1;i++){
-            total=first+second;
-            first=second;
-            second=total;
-        }
-       return total;
+    int l=0;
+    int f=1;
+    int res=0;
+    if(n==0 || n==1){
+        return 1;
+    }
+    for(int i=0;i<n;i++){
+         res=l+f;
+         l=f;
+         f=res;
+    }
+    return res;
     }
 };
+
 
