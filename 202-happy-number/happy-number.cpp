81f6947b1e77f03c770906f3dd9b2badba407ec6
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-17 10:37:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (85.92%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -26,6 +26,30 @@
 
 
 
+// class Solution {
+// public:
+//     bool isHappy(int n) {
+//         if(n==1 || n==7){
+//             return true;
+//         }
+//         while(n>9){
+//             int sum=0;
+//             while(n>0){
+//                 int digit=n%10;
+//                 sum+= digit*digit;
+//                 n/=10;
+//             }
+//             n=sum;
+//             if(n==1 || n==7){
+//                 return true;
+//             }
+//         }
+//         return n==1 || n==7;
+//     }
+// };
+
+
+
 class Solution {
 public:
     bool isHappy(int n) {
@@ -35,8 +59,8 @@ public:
         while(n>9){
             int sum=0;
             while(n>0){
-                int digit=n%10;
-                sum+= digit*digit;
+                int rem=n%10;
+                sum+=rem*rem;
                 n/=10;
             }
             n=sum;
@@ -44,11 +68,8 @@ public:
                 return true;
             }
         }
-        return n==1 || n==7;
+        return n==1||n==7;
     }
 };
-
-
-
 
 
