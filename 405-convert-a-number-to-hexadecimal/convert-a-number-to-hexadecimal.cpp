41578d5a8cd97c94c0038c76e9d790492fefc513
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-17 09:57:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (13.65%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.cpp
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.cpp
@@ -1,19 +1,39 @@
+// class Solution {
+// public:
+//     string toHex(int num) {
+//     string digits="0123456789abcdef";
+//     string res="";
+//     unsigned int n=num;
+//     if(n==0){
+//         return to_string(0);
+//     }
+//     while(n>0){
+//         int rem=n%16;
+//         res=res+digits[rem];
+//         n=n/16;
+//     }
+//     reverse(res.begin(),res.end());
+//     return res;
+//     }
+// };
+
+
+
 class Solution {
 public:
     string toHex(int num) {
-    string digits="0123456789abcdef";
-    string res="";
-    unsigned int n=num;
-    if(n==0){
+     string digits="0123456789abcdef";
+     unsigned int n=num;
+     string res="";
+     if(n==0){
         return to_string(0);
-    }
-    while(n>0){
+     }  
+     while(n>0){
         int rem=n%16;
-        res=res+digits[rem];
+        res+=digits[rem];
         n=n/16;
-    }
-    reverse(res.begin(),res.end());
-    return res;
+     }
+     reverse(res.begin(),res.end());
+     return res;
     }
 };
-
