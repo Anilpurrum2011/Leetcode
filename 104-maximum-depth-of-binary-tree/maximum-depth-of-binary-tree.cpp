d38296e9b51f3ab3967568baa1d1ea193cc4cc3a
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-21 10:39:06 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (75.75%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -15,8 +15,9 @@ public:
         if(root==NULL){
             return 0;
         }
-        int left=maxDepth(root->left);
-        int right=maxDepth(root->right);
-        return 1+max(left,right);
+        // int left=maxDepth(root->left);
+        // int right=maxDepth(root->right);
+        // return 1+max(left,right);
+        return max(maxDepth(root->left),maxDepth(root->right))+1;
     }
 };
