46a0545c5b25f2871111cf070a3ff9e25caaf45f
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-22 23:13:00 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (92.93%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,20 +1,46 @@
+// class Solution {
+// public:
+//     bool checkValidString(string s) {
+//         int low = 0, high = 0;
+//         for (char c : s) {
+//             if (c == '(') {
+//                 low++;
+//                 high++;
+//             } else if (c == ')') {
+//                 if (low > 0) low--;
+//                 high--;
+//             } else { 
+//                 if (low > 0) low--;
+//                 high++;
+//             }
+//             if (high < 0) return false;
+//         }
+//         return low == 0;
+//     }
+// };
+
+
+
 class Solution {
 public:
     bool checkValidString(string s) {
-        int low = 0, high = 0;
-        for (char c : s) {
-            if (c == '(') {
+        int low=0,high=0;
+        for(char c:s){
+            if(c=='('){
                 low++;
                 high++;
-            } else if (c == ')') {
-                if (low > 0) low--;
+            }
+            else if(c==')'){
+                if(low>0)low--;
                 high--;
-            } else { 
-                if (low > 0) low--;
+            }
+            else{
+                if(low>0)low--;
                 high++;
             }
-            if (high < 0) return false;
+            if(high<0)return false;
         }
-        return low == 0;
+        return low==0;
     }
 };
+
