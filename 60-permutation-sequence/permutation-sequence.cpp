8e84e55e5edf12534cefde6ab98d79c3928dc94c
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-22 11:31:26 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (15.37%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,27 +1,27 @@
-class Solution {
-public:
-    string getPermutation(int n, int k) {
-        vector<int>fact(n);
-        fact[0]=1;
-        for(int i=1;i<n;i++){
-          fact[i]=fact[i-1]*i;
-        }
-        vector<int>a;
-        for(int i=1;i<=n;i++){
-            a.push_back(i);
-        }
-        k--;
-        string res="";
-        for(int i=n-1;i>=0;i--){
-            int idx=k/fact[i];
-            res+=to_string(a[idx]);
-            a.erase(a.begin()+ idx);
-            k%=fact[i];
-        }
-        return res;
-    }
+// class Solution {
+// public:
+//     string getPermutation(int n, int k) {
+//         vector<int>fact(n);
+//         fact[0]=1;
+//         for(int i=1;i<n;i++){
+//           fact[i]=fact[i-1]*i;
+//         }
+//         vector<int>a;
+//         for(int i=1;i<=n;i++){
+//             a.push_back(i);
+//         }
+//         k--;
+//         string res="";
+//         for(int i=n-1;i>=0;i--){
+//             int idx=k/fact[i];
+//             res+=to_string(a[idx]);
+//             a.erase(a.begin()+ idx);
+//             k%=fact[i];
+//         }
+//         return res;
+//     }
 
-};
+// };
 
 
 
@@ -52,3 +52,29 @@ public:
 //         return res;
 //     }
 // };
+
+
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int>fact(n);
+        fact[0]=1;
+        for(int i=1;i<n;i++){
+            fact[i]=fact[i-1]*i;
+        }
+        vector<int>a;
+        for(int i=1;i<=n;i++){
+          a.push_back(i);
+        }
+        k--;
+        string res="";
+        for(int i=n-1;i>=0;i--){
+            int idx=k/fact[i];
+            res+=to_string(a[idx]);
+            a.erase(a.begin()+idx);
+            k%=fact[i];
+        }
+       return res;
+    }
+};
