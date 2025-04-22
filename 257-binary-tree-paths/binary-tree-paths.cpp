fa7132f4a203d080f7873162133d9061f0f95f93
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-22 14:40:11 +0530

    Time: 3 ms (29.68%) | Memory: 17.7 MB (49.18%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+//     void dfs(TreeNode* node ,string current,vector<string>& ans){
+//             current+=to_string(node->val);
+//             if(!node->left && !node->right){
+//                 ans.push_back(current);
+//             }
+//             current+="->";
+//             if(node->left) dfs(node->left,current,ans);
+//             if(node->right) dfs(node->right,current,ans);
+//             current.pop_back();
+//     }
+//     vector<string> binaryTreePaths(TreeNode* root) {
+//         vector<string> ans;
+//         if(!root) return ans;
+//         dfs(root,"",ans);
+//         return ans;
+//     }
+// };
+
+
+
+class Solution {
+public:
+    void dfs(TreeNode* root ,string current,vector<string>& ans){
+        current+=to_string(root->val);
+        if(!root->left&&!root->right){
+            ans.push_back(current);
+        }
+        current+="->";
+        if(root->left) dfs(root->left,current,ans);
+        if(root->right) dfs(root->right,current,ans);
+        current.pop_back();
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+      vector<string>ans;
+      if(!root){
+      return ans;
+      }
+      dfs(root,"",ans);
+      return ans;
+    }
+};
