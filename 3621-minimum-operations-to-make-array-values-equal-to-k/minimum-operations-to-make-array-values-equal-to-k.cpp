7861f720f320af1184c95994833b424a877619d5
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-24 00:07:47 +0530

    Time: 8 ms (78.55%) | Memory: 32.3 MB (52.52%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -5,11 +5,15 @@ public:
         int mini = nums[0];
         if (mini < k) return -1; 
         int cnt = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > k) {
+        unordered_set<int>sets;
+        for(auto num:nums){
+            sets.insert(num);
+        }
+        for (int num:sets) {
+            if (num > k) {
                 cnt++;
-                while (i + 1 < nums.size() && nums[i] == nums[i + 1]) 
-                    i++;
+                // while (i + 1 < nums.size() && nums[i] == nums[i + 1]) 
+                //     i++;
             }
         }
         return cnt; 
