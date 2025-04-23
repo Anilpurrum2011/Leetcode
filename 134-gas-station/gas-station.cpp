93e075625ddf5f435e363dadd8654ffb9479c11a
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-23 10:16:50 +0530

    Time: 7 ms (12.12%) | Memory: 112.5 MB (42.43%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -25,25 +25,48 @@
 // };
 
 
+// class Solution {
+// public:
+//     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+//        int totalcost=0;
+//        int totalgas=0;
+//        int total=0;
+//        int start=0;
+//        for(int i=0;i<gas.size();i++){
+//         totalgas+=gas[i];
+//         totalcost+=cost[i];
+//        }
+//        if(totalgas<totalcost)return -1;
+//        for(int i=0;i<gas.size();i++){
+//         total+=(gas[i]-cost[i]);
+//         if(total<0){
+//             total=0;
+//             start=i+1;
+//         }
+//        }
+//        return start;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-       int totalcost=0;
-       int totalgas=0;
-       int total=0;
-       int start=0;
-       for(int i=0;i<gas.size();i++){
-        totalgas+=gas[i];
-        totalcost+=cost[i];
-       }
-       if(totalgas<totalcost)return -1;
-       for(int i=0;i<gas.size();i++){
-        total+=(gas[i]-cost[i]);
-        if(total<0){
-            total=0;
+    int start=0,tank=0,total=0;
+    for(int i=0;i<gas.size();i++){
+        int bal=gas[i]-cost[i];
+        total+=bal;
+        tank+=bal;
+        if(tank<0){
             start=i+1;
+            tank=0;
         }
-       }
-       return start;
+    }
+    if(total>=0){
+        return start;
+    }
+    return -1;
     }
 };
+
