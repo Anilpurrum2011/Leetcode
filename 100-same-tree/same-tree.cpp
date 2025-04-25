d7209eeac8ed80e350fd0cb3a44764cb8801e306
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-25 16:28:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (48.03%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -19,15 +19,28 @@
 // };
 
 
+// class Solution {
+// public:
+//     bool isSameTree(TreeNode* root1, TreeNode* root2) {
+//         if(root1==nullptr && root2==nullptr){
+//             return true;
+//         }
+//         if(!root1 || !root2) return false;
+//         return (root1->val==root2->val)&&isSameTree(root1->left,root2->left) && isSameTree(root1->right,root2->right);
+//     }
+// };
+
+
 class Solution {
 public:
     bool isSameTree(TreeNode* root1, TreeNode* root2) {
-        if(root1==nullptr && root2==nullptr){
+        if(root1==nullptr&& root2==nullptr){
             return true;
         }
-        if(!root1 || !root2) return false;
-        return (root1->val==root2->val)&&isSameTree(root1->left,root2->left) && isSameTree(root1->right,root2->right);
+        if(!root1 || !root2){
+            return false;
+        }
+        return (root1->val==root2->val)&&isSameTree(root1->left,root2->left)&&isSameTree(root1->right,root2->right);
     }
 };
-
 
