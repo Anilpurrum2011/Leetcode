af43bd87207b4f8a83bf60e5d6fe31789ed0bc31
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-04-25 16:04:54 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (24.15%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -9,6 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// class Solution {
+// public:
+//     int height(TreeNode* root){
+//         if(root==nullptr){
+//             return 0;
+//         }
+//         return max(height(root->left),height(root->right))+1;
+//     }
+//     bool isBalanced(TreeNode* root) {
+//         if(root==nullptr){
+//             return true;
+//         }
+//         int lh=height(root->left);
+//         int rh=height(root->right);
+//         if(abs(lh-rh)<=1){
+//             return isBalanced(root->left)&& isBalanced(root->right);
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     int height(TreeNode* root){
@@ -16,16 +38,18 @@ public:
             return 0;
         }
         return max(height(root->left),height(root->right))+1;
+    
     }
     bool isBalanced(TreeNode* root) {
-        if(root==nullptr){
-            return true;
-        }
-        int lh=height(root->left);
-        int rh=height(root->right);
-        if(abs(lh-rh)<=1){
-            return isBalanced(root->left)&& isBalanced(root->right);
-        }
-        return false;
+    if(root==nullptr){
+        return true;
+    }
+    int lh=height(root->left);
+    int rh=height(root->right);
+    if(abs(lh-rh)<=1){
+        return isBalanced(root->left)&&isBalanced(root->right);
+    }
+    return false;
     }
 };
+
