b34dfcd5458c2a471452b82222f65dc795642d9e
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-05-12 09:57:32 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (48.70%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -1,18 +1,42 @@
+// class Solution {
+// public:
+//     int findNumbers(vector<int>& nums) {
+//         int count = 0;
+//         for (int i : nums) {
+//             if (countDigits(i) % 2 == 0) ++count;
+//         } 
+//         return count;
+//     }
+//     int countDigits(int x) {
+//         int cnt = 0;
+//         while (x > 0) {
+//             x /= 10;
+//             ++cnt;
+//         }
+//         return cnt;
+//     }
+// };
+
 class Solution {
 public:
-    int findNumbers(vector<int>& nums) {
-        int count = 0;
-        for (int i : nums) {
-            if (countDigits(i) % 2 == 0) ++count;
-        } 
-        return count;
-    }
-    int countDigits(int x) {
-        int cnt = 0;
-        while (x > 0) {
-            x /= 10;
-            ++cnt;
+    int countdigits(int num){
+        int cnt=0;
+        while(num>0){
+          int rem=num%10;
+          cnt++;
+          num/=10;
         }
         return cnt;
     }
+    int findNumbers(vector<int>& nums) {
+    int n=nums.size();
+    int count=0;
+    for(int i:nums){
+        if(countdigits(i)%2==0){
+            count++;
+        }
+    }
+    return count;
+    }
 };
+
