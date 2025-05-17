73ba0191a2ea4e619680ff465e014ca0361b7b55
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-05-17 22:55:53 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (78.60%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,14 +1,42 @@
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         // for(int i=0; i<nums.size(); i++)
+//         // {
+//         //     for(int j=i+1; j<nums.size(); j++)
+//         //     {
+//         //         if(nums[i]>nums[j])
+//         //         swap(nums[i],nums[j]);
+//         //     }
+//         // }
+//         sort(nums.begin(),nums.end());
+//     }
+// };
+
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // for(int i=0; i<nums.size(); i++)
-        // {
-        //     for(int j=i+1; j<nums.size(); j++)
-        //     {
-        //         if(nums[i]>nums[j])
-        //         swap(nums[i],nums[j]);
-        //     }
-        // }
-        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int zero=0,one=0,two=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0)zero++;
+            else if(nums[i]==1)one++;
+            else two++;
+        }
+        for(int i=0;i<n;i++){
+            if(zero>0){
+                nums[i]=0;
+                zero--;
+            }
+            else if(one>0){
+                nums[i]=1;
+                one--;
+            }
+            else{
+                nums[i]=2;
+                two--;
+            }
+        }
     }
 };
