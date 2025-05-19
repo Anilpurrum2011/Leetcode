fdff49703d35784f5346b4509af54fcbae56e33c
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-05-19 22:18:08 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (8.96%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.cpp
+++ 3321-type-of-triangle/type-of-triangle.cpp
@@ -1,20 +1,32 @@
+// class Solution {
+// public:
+//     string triangleType(vector<int>& nums) {
+//         int a=nums[0],b=nums[1],c=nums[2];
+//         string res="";
+//         if((a+b>c && b+c>a && c+a>b)&&(a==b && a==c)){
+//             res+="equilateral";
+//         }
+//         else if((a+b>c && b+c>a && c+a>b)&& (a==b || b==c || c==a)){
+//             res+="isosceles";
+//         }
+//         else if(a+b>c && b+c>a && c+a>b){
+//             res+="scalene";
+//         }
+//         else{
+//             res+="none";
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     string triangleType(vector<int>& nums) {
-        int a=nums[0],b=nums[1],c=nums[2];
-        string res="";
-        if((a+b>c && b+c>a && c+a>b)&&(a==b && a==c)){
-            res+="equilateral";
-        }
-        else if((a+b>c && b+c>a && c+a>b)&& (a==b || b==c || c==a)){
-            res+="isosceles";
-        }
-        else if(a+b>c && b+c>a && c+a>b){
-            res+="scalene";
-        }
-        else{
-            res+="none";
-        }
-        return res;
+        set<int>s(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());
+        if((nums[0] + nums[1])<=nums[2]) return "none";
+        if(s.size() == 1) return "equilateral";
+        if(s.size() == 2) return "isosceles";
+        return "scalene";
     }
 };
