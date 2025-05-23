4918c03325385c1389bbd4ff1090cf869cad295a
Anil <113875757+Anilpurrum2011@users.noreply.github.com>
2025-05-23 21:14:43 +0530

    Time: 3 ms (5.80%) | Memory: 8.9 MB (13.86%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -4,13 +4,11 @@ public:
         if (haystack.length() < needle.length()) {
             return -1;
         }
-        
         for (int i = 0; i <= haystack.length() - needle.length(); i++) {
             if (haystack.substr(i, needle.length()) == needle) {
                 return i;
             }
-        }
-        
+        } 
         return -1;        
     }
 };
